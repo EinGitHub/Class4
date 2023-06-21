a273ea039874034898d066d6d7e32e957ccf2d74
EinGitHub <137085448+EinGitHub@users.noreply.github.com>
2023-06-20 22:49:54 -0400

    First update

---
+++ mp10.java
@@ -0,0 +1,20 @@
+package Class4;
+import java.util.Scanner;
+class mp10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner kb = new Scanner(System.in);
+		int sum = 0;
+		int num = 0;
+
+		do {
+			sum = sum + num;
+			System.out.println("Please give me pos num ?");
+			num = kb.nextInt();
+		} while (num >= 0);
+
+		System.out.println("the sum of pos num is " + sum);
+	}
+
+}
